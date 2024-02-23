89ff4991a304b7433aabdba23fb6257f544829a7
Baymurat785 <abdimuratovbaymurat03@gmail.com>
2024-02-23 19:17:38 +0500

    Initial commit

---
+++ MachineLearning.swift
@@ -0,0 +1,8 @@
+//
+//  MachineLearning.swift
+//  SeaFood
+//
+//  Created by Baymurat Abdumuratov on 23/02/24.
+//
+
+import Foundation
--- SeaFood/ViewController.swift
+++ SeaFood/ViewController.swift
@@ -6,14 +6,85 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+        
+    }
+    
+    
+    
+    //It's called when the user finishes picking media with the image picker.
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            //here we will captured image, and replace it with the empty imageView
+            imageView.image = userPickedImage
+            
+            
+            //CIImage is specialized for image processing tasks using Core Image
+            guard let ciimage = CIImage(image: userPickedImage) else{
+                fatalError("Coudl not convert UIImage into CIImage")
+            }
+            detect(image: ciimage)
+        }
+        imagePicker.dismiss(animated: true)
     }
 
-
+    
+    
+    func detect(image: CIImage){
+        
+        // Loading Core ML model
+        guard let model = try? VNCoreMLModel(for: MLModel(contentsOf: Inceptionv3.urlOfModelInThisBundle)) else{
+            fatalError("Can't load ML model")
+        }
+        
+        
+        
+        // Creating a request to run the model
+        let request = VNCoreMLRequest(model: model) { request , error in
+            guard let results = request.results as? [VNClassificationObservation] else{
+                fatalError("Model failed to process the image")
+            }
+            
+        
+            print(results)
+            if let firstResult = results.first{
+                if firstResult.identifier.contains("hotdog"){
+                    self.navigationItem.title = "hotdog"
+                }else{
+                    self.navigationItem.title = "not hotdog"
+                }
+            }
+        }
+        
+        
+        //VNImageRequestHandler is a class provided by the Vision framework in iOS. It is used to process images through the Vision framework
+        let handler = VNImageRequestHandler(ciImage: image)
+        do{
+            try! handler.perform([request])
+        }catch{
+            print(error)
+        }
+    }
+    
+    
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true)
+        
+    }
+    
 }
 
